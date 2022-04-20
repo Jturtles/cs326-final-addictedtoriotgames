d0d060b074c939692fbc5fa98b2190e22d97fb31
Jturtles <jasonchen765@gmail.com>
2022-04-19 23:26:12 -0400

    heroku deploy

---
+++ deploy/server/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import { UserDatabase } from './user-db.js';
+
+class UserServer {
+  constructor(dburl) {
+    this.dburl = dburl;
+    this.app = express();
+  }
+
+  async initRoutes() {
+    // Note: when using arrow functions, the "this" binding is lost.
+    const self = this;
+
+    this.app.get('/user/create', async (req, res) => {
+      try {
+        const { id, name, email, username, pictures  } = req.query;
+        const user = await self.db.createUser(id, name, email, username, pictures);
+        res.send(JSON.stringify(user));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    this.app.get('/user/read', async (req, res) => {
+      try {
+        const { id } = req.query;
+        const user = await self.db.readUser(id);
+        res.send(JSON.stringify(user));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    this.app.get('/user/update', async (req, res) => {
+      try {
+        const { id, name, email, username, pictures } = req.query;
+        const user = await self.db.updateUser(id, name, email, username, pictures);
+        res.send(JSON.stringify(user));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    this.app.get('/user/delete', async (req, res) => {
+      try {
+        const { id } = req.query;
+        const user = await self.db.deleteUser(id);
+        res.send(JSON.stringify(user));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    this.app.get('/user/all', async (req, res) => {
+      try {
+        const user = await self.db.readAllUser();
+        res.send(JSON.stringify(user));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+  }
+
+  async initDb() {
+    this.db = new UserDatabase(this.dburl);
+    await this.db.connect();
+  }
+
+  async start() {
+    await this.initRoutes();
+    await this.initDb();
+    const port = process.env.PORT || 3000;
+    this.app.listen(port, () => {
+      console.log(`UserServer listening on port ${port}!`);
+    });
+  }
+}
+
+const server = new UserServer(process.env.DATABASE_URL);
+server.start();
